0a5d609b8f357c907667ce8063fad478bb01e449
YiHerngOng <ongyi@oregonstate.edu>
2019-01-09 00:58:29 -0800

    motion planning done

--- scripts/barrettwam_moveit.py
+++ scripts/barrettwam_moveit.py
@@ -113,7 +113,7 @@ def sampling_r_Poses(increment, num): # for roll
 	temp = base_pose[:]
 	for j in range(num/2):
 		if j > 0:
-			temp[2] += 0.015
+			temp[2] += 0.02
 		temp[4] += increment
 		temp1 = temp[:]
 		poses.append(temp1)
@@ -121,7 +121,7 @@ def sampling_r_Poses(increment, num): # for roll
 
 def sampling_p_Poses(increment, num): # for pitch only
 	# base pose becomes 
-	base_pose = [0.6972,0,0.86,0,90,0]
+	base_pose = [0.7972,0,0.86,0,90,0]
 	poses = []
 	temp = base_pose[:]
 	temp[4] -= increment
@@ -130,6 +130,7 @@ def sampling_p_Poses(increment, num): # for pitch only
 	# pdb.set_trace()
 	temp = base_pose[:]
 	for n in range(num-1):
+		temp[2] += 0.02
 		temp[4] += increment
 		temp1 = temp[:]
 		poses.append(temp1)
@@ -151,7 +152,16 @@ def sampling_w_Poses(increment, num): # for yaw
 		poses.append(temp1)
 	return poses
 
-
+def write_file(file, Robot):
+	for a in range(len(Robot.plan.joint_trajectory.points)):
+		temp = Robot.plan.joint_trajectory.points[a].positions
+		for b in range(len(temp)):
+			file.write(str(temp[b]))
+			file.write(",")
+		if a == (len(Robot.plan.joint_trajectory.points) - 1):
+			print "yes"
+			file.write("f")
+		file.write("\n")
 if __name__ == '__main__':
 	x = sampling_x_Poses(0.02, 6)
 	y = sampling_y_Poses(0.02, 4)
@@ -163,64 +173,53 @@ if __name__ == '__main__':
 
 	Robot = robot("barrett_wam")
 	Robot.planner_type()
+	file_base = open("joint_angles_base.csv","wb")
 	Robot.move_to_Goal(base_pose)
+	write_file(file_base, Robot)
+	file_base.close()
+
+	rospy.sleep(2)
+
+	file_x = open("joint_angles_x.csv","wb")
 	for i in range(len(x)):
 		rospy.loginfo("Planning and executing x_{}".format(i))
 		Robot.move_to_Goal(x[i])
-	rospy.sleep(2)
+		write_file(file_x, Robot)
+	file_x.close()
+
+	file_y = open("joint_angles_y.csv","wb")
 	for j in range(len(y)):
 		rospy.loginfo("Planning and executing y_{}".format(j))
 		rospy.loginfo("Current Y pose {}".format(y[j]))
-		Robot.move_to_Goal(y[j])	
+		Robot.move_to_Goal(y[j])
+		write_file(file_y, Robot)
+	file_y.close()	
 
+	file_z = open("joint_angles_z.csv","wb")
 	for k in range(len(z)):
 		rospy.loginfo("Planning and executing z_{}".format(k))
 		Robot.move_to_Goal(z[k])
+		write_file(file_z, Robot)
+	file_z.close()
 
+	file_r = open("joint_angles_r.csv","wb")
 	for l in range(len(r)):
 		rospy.loginfo("Planning and executing r_{}".format(l))
 		Robot.move_to_Goal(r[l])
+		write_file(file_r, Robot)
+	file_r.close()
 
+	file_p = open("joint_angles_p.csv","wb")
 	for m in range(len(p)):
 		rospy.loginfo("Planning and executing p_{}".format(m))
 		Robot.move_to_Goal(p[m])
+		write_file(file_p, Robot)
+	file_p.close()
 
+	file_w = open("joint_angles_w.csv","wb")
 	for n in range(len(w)):
 		rospy.loginfo("Planning and executing w_{}".format(n))
 		Robot.move_to_Goal(w[n])
+		write_file(file_w, Robot)
+	file_w.close()
 	# pdb.set_trace()
-
-	# moveit_commander.roscpp_initialize(sys.argv)
-	# rospy.init_node("barrett_wam_move_group", anonymous=True)
-	# robot = moveit_commander.RobotCommander()
-	# scene = moveit_commander.PlanningSceneInterface()
-	# group_name = "barrett_wam"
-	# move_group = moveit_commander.MoveGroupCommander("arm")
-	# move_group.set_planner_id("RRTConnectkConfigDefault")
-	# display_trajectory_publisher = rospy.Publisher("/move_group/display_planned_path", moveit_msgs.msg.DisplayTrajectory, queue_size=20)
-	# display_trajectory = moveit_msgs.msg.DisplayTrajectory()
-	# display_trajectory.trajectory_start = robot.get_current_state()
-	# # print robot.get_current_state()
-	# pose_goal = geometry_msgs.msg.Pose()
-	# # print move_group.get_current_pose()
-	# rospy.set_param('/move_group/trajectory_execution/allowed_start_tolerance', 0.0)
-	# rospy.sleep(2)
-	# quat = tf.transformations.quaternion_from_euler(0, math.radians(90), 0)
-	# pose_goal.position.x = 0.6972
-	# pose_goal.position.y = 0.0
-	# pose_goal.position.z = 0.80
-	# pose_goal.orientation.x = quat[0]
-	# pose_goal.orientation.y = quat[1]
-	# pose_goal.orientation.z = quat[2]
-	# pose_goal.orientation.w = quat[3]
-	# move_group.set_pose_target(pose_goal)
-	# plan = move_group.plan()
-	# move_group.set_planning_time(5)
-	# move_group.go(wait=True)
-	# move_group.execute(plan, wait=True)
-	# move_group.stop()
-	# move_group.clear_pose_targets()
-	# display_trajectory.trajectory.append(plan)
-	# print plan
-	# # # Publish
-	# display_trajectory_publisher.publish(display_trajectory)
